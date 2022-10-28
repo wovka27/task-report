2f215e9b3cf897e7d476c64d45f32f888a0af155
Vladimir Demyanchuk <demyanchuk-vy@72dom.com>
2022-10-28 15:36:20 +0500

    рефакторинг

--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
 import TaskReport from '../js/TaskReport.js'
 
 document.addEventListener('DOMContentLoaded', () => new TaskReport({
-    result: 'result',
-    clear: 'clear',
-    copy: 'copy',
     taskList: 'task-list',
 }).init())
