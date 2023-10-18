6860cbc4c38bdc7163ee4e8a12a5fe1c40e596ff
Vladimir Demyanchuk <demyanchuk-vy@dom-digital.pro>
2023-10-18 13:38:43 +0500

    fix

--- js/utils.js
+++ js/utils.js
@@ -1,9 +1,9 @@
 export const date = {
-    dayWeek: new Intl.DateTimeFormat('ru', {weekday: 'long'}).format(),
-    today: new Intl.DateTimeFormat('ru').format(),
+  dayWeek: new Intl.DateTimeFormat('ru', {weekday: 'long'}).format(),
+  today: new Intl.DateTimeFormat('ru').format(),
 };
 
-export const DAY_WEEK = date.dayWeek;
+export const DAY_WEEK = date.dayWeek.charAt(0).toUpperCase() + date.dayWeek.slice(1);
 
 export const checkBrowser = (type) => navigator.userAgent.toLowerCase().match(type)
 
@@ -11,13 +11,13 @@ export const TASK_REPORT = "tasks-report";
 export const ARCHIVE_LISTS = "archive-lists";
 
 export const getValues = (items, valCb) =>
-    items?.map((item) => valCb(item)).join("");
+  items?.map((item) => valCb(item)).join("");
 
 export const writeClipboard = async (val) => {
-    if (!Boolean(val)) {
-        return;
-    }
-    await navigator.clipboard.writeText(val);
+  if (!Boolean(val)) {
+    return;
+  }
+  await navigator.clipboard.writeText(val);
 };
 
 /**
@@ -30,10 +30,10 @@ export const writeClipboard = async (val) => {
  *      }}
  */
 export const getChangeTask = (type, cb) => {
-    const {data: tasks} = useStorage();
-    const result = tasks[type]((item) => cb(item));
-    const setResult = (data = result) => storage.set(TASK_REPORT, data);
-    return {tasks, result, setResult};
+  const {data: tasks} = useStorage();
+  const result = tasks[type]((item) => cb(item));
+  const setResult = (data = result) => storage.set(TASK_REPORT, data);
+  return {tasks, result, setResult};
 };
 
 /**
@@ -48,18 +48,18 @@ export const noDuplicate = (value) => [...new Set(value)];
  * @param render {(tasks: Array<{id: number | string, value: string}>) => void}
  */
 export const setStorageTask = (value, render) => {
-    storage.set(
-        TASK_REPORT,
-        noDuplicate([...(storage.get(TASK_REPORT) ?? []), value])
-    );
-    render(storage.get(TASK_REPORT));
+  storage.set(
+    TASK_REPORT,
+    noDuplicate([...(storage.get(TASK_REPORT) ?? []), value])
+  );
+  render(storage.get(TASK_REPORT));
 };
 
 export const storage = {
-    get: (name) => JSON.parse(localStorage.getItem(name)),
-    set: (name, value) =>
-        value && localStorage.setItem(name, JSON.stringify(value)),
-    delete: (name) => localStorage.removeItem(name),
+  get: (name) => JSON.parse(localStorage.getItem(name)),
+  set: (name, value) =>
+    value && localStorage.setItem(name, JSON.stringify(value)),
+  delete: (name) => localStorage.removeItem(name),
 };
 
 /**
@@ -69,16 +69,16 @@ export const storage = {
  * @param remove {boolean}
  */
 export const setEventListeners = (target, handlers = [], remove = false) => {
-    const eventNames = ["input", "keydown", "blur"];
-    if (!remove) {
-        eventNames.forEach((name, index) =>
-            target.addEventListener(name, handlers[index])
-        );
-    } else {
-        eventNames.forEach((name, index) =>
-            target.removeEventListener(name, handlers[index])
-        );
-    }
+  const eventNames = ["input", "keydown", "blur"];
+  if (!remove) {
+    eventNames.forEach((name, index) =>
+      target.addEventListener(name, handlers[index])
+    );
+  } else {
+    eventNames.forEach((name, index) =>
+      target.removeEventListener(name, handlers[index])
+    );
+  }
 };
 
 /**
@@ -86,11 +86,11 @@ export const setEventListeners = (target, handlers = [], remove = false) => {
  * @param name {string}
  */
 export const useStorage = (name = TASK_REPORT) => {
-    const data = storage.get(name) || [];
-    const setData = (data) => storage.set(name, data);
-    const deleteData = () => storage.delete(name);
+  const data = storage.get(name) || [];
+  const setData = (data) => storage.set(name, data);
+  const deleteData = () => storage.delete(name);
 
-    return {data, setData, deleteData};
+  return {data, setData, deleteData};
 };
 
 /**
@@ -98,15 +98,15 @@ export const useStorage = (name = TASK_REPORT) => {
  * @param cb {(animEnd: boolean) => void}
  */
 export const afterAnimationEnd = (cb) => {
-    let flag = false;
-    const handler = (e) => {
-        if (e.returnValue) {
-            flag = !flag;
-            cb(flag);
-            document.removeEventListener("animationend", handler);
-        }
-    };
-    document.addEventListener("animationend", handler);
+  let flag = false;
+  const handler = (e) => {
+    if (e.returnValue) {
+      flag = !flag;
+      cb(flag);
+      document.removeEventListener("animationend", handler);
+    }
+  };
+  document.addEventListener("animationend", handler);
 };
 
 /**
@@ -115,84 +115,84 @@ export const afterAnimationEnd = (cb) => {
  * @param {string} selector
  */
 export const grabScroll = (selector) => {
-    const $el = document.querySelector(selector)
+  const $el = document.querySelector(selector)
 
-    /**
-     * Отключение всех событий у потомков при скролле по клику
-     * @param flag
-     */
-    const noClick = (flag) => {
-        Array.from($el.children).forEach((item) => {
-            if (flag) {
-                item.style.pointerEvents = "none";
-            } else {
-                item.style.pointerEvents = "auto";
-            }
-        });
-    };
+  /**
+   * Отключение всех событий у потомков при скролле по клику
+   * @param flag
+   */
+  const noClick = (flag) => {
+    Array.from($el.children).forEach((item) => {
+      if (flag) {
+        item.style.pointerEvents = "none";
+      } else {
+        item.style.pointerEvents = "auto";
+      }
+    });
+  };
 
-    /**
-     *
-     * @param e {MouseEvent}
-     */
-    const down = (e) => {
-        e.preventDefault();
-        $el.style.cursor = 'grab'
-        $el.savedPageX = e.pageX;
-        $el.savedScrollLeft = $el.scrollLeft;
-    };
+  /**
+   *
+   * @param e {MouseEvent}
+   */
+  const down = (e) => {
+    e.preventDefault();
+    $el.style.cursor = 'grab'
+    $el.savedPageX = e.pageX;
+    $el.savedScrollLeft = $el.scrollLeft;
+  };
 
-    const up = () => {
-        $el.style.cursor = 'default'
-        $el.savedPageX = null
-        noClick(false);
+  const up = () => {
+    $el.style.cursor = 'default'
+    $el.savedPageX = null
+    noClick(false);
+  }
+  /**
+   *
+   * @param e {MouseEvent}
+   */
+  const move = (e) => {
+    e.preventDefault();
+    if (!$el.savedPageX) return;
+    noClick($el.savedPageX);
+    $el.scrollLeft = $el.savedScrollLeft + $el.savedPageX - e.pageX;
+    $el.style.cursor = 'grabbing'
+  }
+
+  const leave = e => {
+    if ($el.savedPageX) {
+      $el.savedPageX = null;
+      return
     }
-    /**
-     *
-     * @param e {MouseEvent}
-     */
-    const move = (e) => {
-        e.preventDefault();
-        if (!$el.savedPageX) return;
-        noClick($el.savedPageX);
-        $el.scrollLeft =  $el.savedScrollLeft + $el.savedPageX - e.pageX;
-        $el.style.cursor = 'grabbing'
-    }
+  }
 
-    const leave = e => {
-        if ($el.savedPageX) {
-            $el.savedPageX = null;
-            return
-        }
-    }
+  /**
+   *
+   * @param e {WheelEvent & {wheelDelta: number}}
+   */
+  const mousewheel = e => {
+    e.preventDefault();
+    if ($el.savedPageX) return;
+    const delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+    $el.scrollLeft -= delta * 100;
+    $el.savedScrollLeft = $el.scrollLeft
+  }
 
-    /**
-     *
-     * @param e {WheelEvent & {wheelDelta: number}}
-     */
-    const mousewheel = e => {
-        e.preventDefault();
-        if ($el.savedPageX) return;
-        const delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-        $el.scrollLeft -= delta * 100;
-        $el.savedScrollLeft = $el.scrollLeft
-    }
+  /**
+   *
+   * @type {Parameters<(type: keyof DocumentEventMap, listener: (this:Element, ev: any) => any, options?: boolean | AddEventListenerOptions) => void>[]}
+   */
+  const events = [
+    ['mouseleave', leave],
+    ["mousemove", move],
+    ['mousedown', down],
+    ['mouseup', up],
+    [checkBrowser('firefox') ? 'DOMMouseScroll' : 'mousewheel', mousewheel]
+  ]
 
-    /**
-     *
-     * @type {Parameters<(type: keyof DocumentEventMap, listener: (this:Element, ev: any) => any, options?: boolean | AddEventListenerOptions) => void>[]}
-     */
-    const events = [
-        ['mouseleave', leave],
-        ["mousemove", move],
-        ['mousedown',  down],
-        ['mouseup', up],
-        [checkBrowser('firefox') ? 'DOMMouseScroll' : 'mousewheel', mousewheel]
-    ]
-
-    for (const eventArgs of events) {
-        $el.addEventListener(...eventArgs);
-    }
+  for (const eventArgs of events) {
+    $el.addEventListener(...eventArgs);
+  }
 }
 
 /**
@@ -200,16 +200,6 @@ export const grabScroll = (selector) => {
  * @param {string} text
  * @return {string}
  */
-export const textDivider = (text) => {
-    const textArr = text.split(" ");
-    const startArgs = textArr.findIndex((item) => item.match(/:+$/));
-    const textArgs =
-        startArgs !== -1 &&
-        textArr
-            .slice(startArgs + 1, textArr.length)
-            .map((item) => `   - ${item}`)
-            .join("\n");
-    return textArgs
-        ? `${textArr.slice(0, startArgs + 1).join(" ")}\n${textArgs}`
-        : text;
-};
+export const textDivider = (text) => text.split(" ").join("\n") ?? text
+
+
